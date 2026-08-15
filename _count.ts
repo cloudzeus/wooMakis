73b90bcc76396de37718144e317b29d3071c3788
Giannis Kozyris <gkozyris@i4ria.com>
2026-08-15 12:02:03 +0300

    chore: drop stray _count.ts scratch file, ignore _*.ts probes

--- _count.ts
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config'
-import { prisma } from './src/lib/prisma'
-async function main() {
-  console.log('MediaAsset mirrored:', await prisma.mediaAsset.count({ where: { mirroredAt: { not: null } } }))
-}
-main().finally(() => prisma.$disconnect())
